ab1e387326c29f5ece8cba8712e600d818af2ee8
nrp <naruhpak.210@mail.kmutt.ac.th>
2020-10-03 23:39:52 +0700

    add confirm screen

---
+++ lib/screens/add/component/confirm_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ConfirmScreen extends StatefulWidget {
+  ConfirmScreen({Key key}) : super(key: key);
+
+  @override
+  _ConfirmScreenState createState() => _ConfirmScreenState();
+}
+
+class _ConfirmScreenState extends State<ConfirmScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       
+    );
+  }
+}
