792af12aca4ed9d38f3afd5a571e5df5c02d26cb
Khemakorn M <chynurt@Gmail.com>
2020-11-13 16:06:15 +0700

    add page ying

--- lib/screens/account/components/body.dart
+++ lib/screens/account/components/body.dart
@@ -1,10 +1,48 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sharelymeter/components/rounded_button.dart';
+import 'package:sharelymeter/screens/sharelymeter.dart';
 import 'package:sharelymeter/service/auth.dart';
 import 'info.dart';
 import 'package:sharelymeter/database/database.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class Retrieved{
+  //Khemmy add
+  String email = '';
+  String firstname = '';
+  String lastname = '';
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Widget build(BuildContext context) {
+    final User user = auth.currentUser;
+    final uid = user.uid;
+    CollectionReference users = FirebaseFirestore.instance.collection('userInfo');
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(user.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.hasError) {
+          print("sth went wrong");
+          return Text("something went wrong");
+
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data.data();
+          print("Full Name: ${data['firstname']} ${data['lastname']}");
+          return Text("Full Name: ${data['firstname']} ${data['lastname']}");
+        }
+        print("Loading");
+        return Text("Loading");
+      },
+    );
+  }
+}
 
 class Body extends StatefulWidget {
   @override
@@ -13,7 +51,6 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
   final AuthService _auth = AuthService();
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -33,5 +70,6 @@ class _BodyState extends State<Body> {
         ],
       ),
     );
+    FutureBuilder();
   }
 }
